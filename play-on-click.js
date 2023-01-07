627a3d7fa6bb11dde56751b14e4cffb3a13979d8
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2023-01-07 12:07:19 +0530

    Update play-on-click.js

--- play-on-click.js
+++ play-on-click.js
@@ -12,24 +12,6 @@ AFRAME.registerComponent("play-on-click", {
     window.addEventListener("click", this.onClick);
   },
   onClick: function(evt) {
-    if (!this.videoEl) {
-      return;
-    }
-
-    var isPlaying = this.el.getAttribute("play-on-click").isPlaying;
-
-    this.el.object3D.visible = true;
-
-    if (!isPlaying) {
-      this.el.setAttribute("play-on-click", {
-        isPlaying: true
-      });
-      this.videoEl.play();
-    } else {
-      this.el.setAttribute("play-on-click", {
-        isPlaying: false
-      });
-      this.videoEl.pause();
-    }
+   
   }
 });
